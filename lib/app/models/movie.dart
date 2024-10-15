630a505b2aef611c14650c1cc990b1ec868626af
ikyrie <mah.c5r@gmail.com>
2024-10-15 17:40:53 -0300

    Video 4.1

---
+++ lib/app/models/movie.dart
@@ -0,0 +1,15 @@
+class Movie {
+  final int id;
+  final String title;
+  final String imageUrl;
+  final String releaseDate;
+  final String overview;
+
+  Movie({
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+    required this.releaseDate,
+    required this.overview,
+  });
+}
