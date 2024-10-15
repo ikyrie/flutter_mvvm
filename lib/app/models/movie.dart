4ddbd79ce3717be230acc8b177b6c04840e7b381
ikyrie <mah.c5r@gmail.com>
2024-10-15 19:50:57 -0300

    Video 4.4

--- lib/app/models/movie.dart
+++ lib/app/models/movie.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class Movie {
   final int id;
   final String title;
@@ -12,4 +14,28 @@ class Movie {
     required this.releaseDate,
     required this.overview,
   });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'title': title,
+      'imageUrl': imageUrl,
+      'releaseDate': releaseDate,
+      'overview': overview,
+    };
+  }
+
+  factory Movie.fromMap(Map<String, dynamic> map) {
+    return Movie(
+      id: map['id'] as int,
+      title: map['title'] as String,
+      imageUrl: map['poster_path'] as String,
+      releaseDate: map['release_date'] as String,
+      overview: map['overview'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Movie.fromJson(String source) => Movie.fromMap(json.decode(source) as Map<String, dynamic>);
 }
--- lib/app/services/search_movies_service.dart
+++ lib/app/services/search_movies_service.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:cinetopia/app/models/movie.dart';
 import 'package:http/http.dart' as http;
 
@@ -11,7 +13,9 @@ class SearchMoviesService {
       final response = await http.get(Uri.parse(popularMoviesUrl), headers: { 'accept': 'application/json', 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NWZjZDQ5NTlhNGExMWZmZWM3MTljYmJjMWQxMmZhOCIsIm5iZiI6MTcyODU3ODAwNi4xNzQzNCwic3ViIjoiNjcwODAwZWJlNDZhMTJhMTk0MWE0NjVhIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9._06LTNC6hdVGFU6D8-STJzrArO8NcR8Yrz07Ah-ehDA' },);
 
       if (response.statusCode == 200) {
-        print(response.body);
+        for (dynamic movie in json.decode(response.body)['results']) {
+          movieList.add(Movie.fromMap(movie));
+        }
       }
       return movieList;
     } catch (e) {
