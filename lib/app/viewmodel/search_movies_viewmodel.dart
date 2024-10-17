0970b6ff07d2e2f5fe8f2c956cad8b465b12cad4
ikyrie <mah.c5r@gmail.com>
2024-10-17 17:36:42 -0300

    Video 5.4

--- lib/app/viewmodel/search_movies_viewmodel.dart
+++ lib/app/viewmodel/search_movies_viewmodel.dart
@@ -11,5 +11,16 @@ class SearchMoviesViewmodel {
     return _moviesList;
   }
 
+  Future<List<Movie>> searchForMovie(String query) async {
+    if (query.isEmpty) {
+      _moviesList = await getPopularMovies();
+    } else {
+      final SearchMoviesService searchMovie = SearchForMovie(query: query);
+      _moviesList = await searchMovie.getMovies();
+    }
+    
+    return _moviesList;
+  }
+
   List<Movie> get moviesList => _moviesList;
 }
