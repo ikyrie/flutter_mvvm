18b90314c4308fb8e607a1fe0443179fdf3ce0d5
ikyrie <mah.c5r@gmail.com>
2024-10-17 19:01:39 -0300

    Video 6.3

--- lib/ui/screens/movie_details.dart
+++ lib/ui/screens/movie_details.dart
@@ -13,7 +13,55 @@ class MovieDetails extends StatelessWidget {
         backgroundColor: Colors.transparent,
       ),
       backgroundColor: Color(0xFF120326),
-      body: Center(child: Text(movie.title),),
+      body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Image.asset(
+                "assets/movie.png",
+                width: 80,
+                height: 80,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 40),
+                child: Text(
+                  movie.title,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 40),
+                    width: 176,
+                    height: 235,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                          image: NetworkImage(movie.getMovieCoverUrl()),
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16),
+                child: Text(
+                  "Descrição",
+                  textAlign: TextAlign.start,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                ),
+              ),
+              Text(movie.overview),
+            ],
+                    ),
+          ),
+      ),
     );
   }
 }
